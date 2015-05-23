6b53479ae4149d1145e536eb4f14196a5c799d22
Kenji Niinuma <7th5jack@gmail.com>
2015-05-24 02:33:15 +0900

    none
    none

--- vr/sample-002/index.js
+++ vr/sample-002/index.js
@@ -5,7 +5,7 @@ jThree(function(j3){
 	j3("#mesh0").animate( { rotateY: "+=5" }, 500000 );
 	j3("#mesh2").animate( { rotateY: "+=50" }, 500000 );
 
-	j3.Stats();
+//	j3.Stats();
     j3.MobileVR();
 	
 },
