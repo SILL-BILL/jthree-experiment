8c98172dfabdfff7b45e661719073e0247d0d222
SILL-BILL <7th5jack@gmail.com>
2015-07-21 22:23:53 +0900

    none

---
+++ vr/sample-014/index.js
@@ -0,0 +1,14 @@
+jThree( function( j3 ) {
+
+	$( "#loading" ).remove();
+
+
+	j3.Trackball();
+//	j3.MobileVR();
+//	j3.Stereo();
+	j3.Stats();
+
+},
+function() {
+	alert( "このブラウザはWebGLに対応していません。" );
+} );
